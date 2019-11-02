5d143593cafc3550c8744caca9a13787243d46c2
Anna Kuszal <anna.kuszal@o2.pl>
2019-11-02 19:52:27 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const computerMove = `kamień`;
+const playerMove = `papier`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
